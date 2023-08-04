216dcc9ed6bd2f074b0a54379417ed867acd3a0c
patrickfay <faypa01@gettysburg.edu>
2023-08-04 16:47:59 -0400

    fix: NdsButtonComponent test

--- libs/notes-design-system/src/lib/components/button/button.component.spec.ts
+++ libs/notes-design-system/src/lib/components/button/button.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ButtonComponent } from './button.component';
+import { NdsButtonComponent } from './button.component';
 
-describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+describe(NdsButtonComponent.name, () => {
+  let component: NdsButtonComponent;
+  let fixture: ComponentFixture<NdsButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ButtonComponent],
+      imports: [NdsButtonComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(NdsButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
