433565f800c313b9bf2a1b430f223a8fd4e8ed89
patrickfay <faypa01@gettysburg.edu>
2023-08-04 16:22:03 -0400

    feat: add angular test app & angular dependencies

---
+++ apps/test-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'notes-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-app';
+}
